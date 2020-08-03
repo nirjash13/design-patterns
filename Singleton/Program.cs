2e783aa6df7a5c5841eafc7ca6c3e47b8eb1f5b3
Safkat Nirjash <nirjash.buet@gmail.com>
2020-08-03 18:09:02 -0400

    Added Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatterns.Singleton;
+using System;
+
+namespace Design_Patterns
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Testing the world of Singletons!");
+
+      //Get object instance for the first time.
+      var object1 = SingletonObject.Instance();
+      Console.WriteLine($"Instance count: {object1.GetInstanceCount()}");
+
+      //Get object instance for the second time.
+      var object2 = SingletonObject.Instance();
+      Console.WriteLine($"Instance count: {object2.GetInstanceCount()}");
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Singleton/SingletonObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DesignPatterns.Singleton
+{
+  public class SingletonObject
+  {
+    private static readonly SingletonObject instance = new SingletonObject();
+    private static readonly int instanceCount;
+    static SingletonObject()
+    {
+      instanceCount += 1;
+    }
+
+    private SingletonObject()
+    {
+
+    }
+
+    public static SingletonObject Instance()
+    {
+      return instance;
+    }
+
+    public int GetInstanceCount()
+    {
+      return instanceCount;
+    }
+  }
+}
